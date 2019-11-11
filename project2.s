23602db88030a5bf28bc5642a1657593b29481b2
Spencer Lyles <spencerlyles@Spencers-MacBook-Pro.local>
2019-11-11 16:37:50 -0500

    Added sum and beginning of the for loop

--- project2.s
+++ project2.s
@@ -4,6 +4,7 @@
 	message: .asciiz "Input: \n"
 	message1: .asciiz "\nOutput: "
 	message2: .asciiz "\nInvalid input"
+	sum: .word 0
 
 .text # for instructions
 main:
@@ -17,7 +18,7 @@ main:
 	# read a string to operate on
 	li $v0, 8
 	la $a0, str
-	li $a1, 11 # space for 1001 characters
+	li $a1, 11 # space for 11(1001) characters
 	syscall
 	
 	move $t7, $a0 # moves string into $t7 register
@@ -28,9 +29,16 @@ main:
 	syscall
 	
 	# prints character at index 0
-	li $t1, 10 # initialize register $t1 to 1000
+	li $t1, 10 # initialize register $t1 to 10 (1000)
 	li $t2, 0 # initialize byte number to 0
 	
+	lw $t9, sum # loads sum into register
+	
+	li $k0, 0 # puts zero in total sum
+	for_loop:
+	blt $t1, 1, exit # Branch less than 10 < 1 (1000 < 1)
+	
+    lbu $a0, 0($t7) # Loads byte 0 of $t7 (str)
 	
 	
 	
